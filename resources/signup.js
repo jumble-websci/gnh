8be8dc3c2ecf4acd07305d3153a0fe03ca78ec98
liuj-42 <liuxjames88@gmail.com>
2021-10-04 23:36:19 -0400

    create the form page
    no css for the form so far, no form validation, just a form.

---
+++ resources/signup.js
@@ -0,0 +1,8 @@
+// this file contains javascript for form validation and dynamically changing the signup page
+// it might also include ajax calls later
+
+function validate(form) {
+    var isValid = true;
+    var msg = "Form is valid";
+    alert("you clicked the owo button");
+}
