81cc4ce82bb09d3a23851753cc582518a05ef493
nothingexessive <liuxjames88@gmail.com>
2021-10-07 16:07:51 -0400

    prettyed up ajax

--- resources/signup.js
+++ resources/signup.js
@@ -73,24 +73,44 @@ function check() {
     }
 }
 
+function weatherCheck(temp1, temp2) {
+    if (temp2 > temp1) {
+        return "decrease";
+    } else if (temp2 < temp1) {
+        return "increase";
+    } else {
+        return "won't really change";
+    }
+}
+
 $(document).ready(function() {
     $.ajax({
         type:"POST",
-        url:"https://api.openweathermap.org/data/2.5/onecall?lat=42.73&lon=-73.68&exclude=minutely,hourly,daily,alerts&units=imperial&appid=904329b4887857a349c608a92182b8e6",
+        url:"https://api.openweathermap.org/data/2.5/onecall?lat=42.73&lon=-73.68&exclude=minutely,hourly,alerts&units=imperial&appid=904329b4887857a349c608a92182b8e6",
         dataType:"json",
         success: function(data, status) {
             //do stuff here
-            output = "<p>";
-            output += data.current.temp +"&deg;";
-            output += "</p>";
-            output += "<p>";
-            output += "today's weather looks like: " + data.current.weather[0].description;
-            output += "</p>";
+            output = "";
+
+            output += '<span class="icon">';
+            output += '<img width="84" height="84" src="https://openweathermap.org/img/wn/'+    data.current.weather[0].icon + '@2x.png"> </span>';
+            output += '<span class="desc">';
+            output += '<span class="temp">' + data.current.temp +'&deg; &nbsp;'+ data.current.weather[0].description + '</span>';
+            output += '<span class="hl">';
+            output += '<span class="hl-labels"> Feels like:&nbsp;</span>';
+            output += '<span>' + data.current.feels_like + '&deg; &nbsp; </span>';
+            output += '<span class="hl-labels"> High:&nbsp;</span>';
+            output += '<span>' + data.daily[0].temp.max +'&deg; &nbsp;</span>';
+            output += '<span class="hl-labels"> Low:&nbsp; </span>';
+            output += '<span>' + data.daily[0].temp.min +'&deg; &nbsp;</span>';
+            output += '</span>';
+            output += '</span>';
+
             console.log(data);
-            // console.log(data.current.weather[0].description);
             $('#weather').html(output);
         }, error: function(msg) {
             alert("There was a problem: " + msg.status + " " + msg.statusText);
         }
     });
 });
+
